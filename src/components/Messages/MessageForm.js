4d9993122c78aee4281e4aa1089321b23581b66e
KnifeParty12 <flashfunk01@gmail.com>
2019-10-08 16:06:55 +0530

    Added media file in state and mimetype Validation
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -66,6 +66,10 @@ class MessageForm extends React.Component {
 
     };
 
+    uploadFile = (file,metadata) => {
+        console.log(file,metadata);
+    }
+
 
     render() {
         const {errors,message,loading,modal} = this.state;
@@ -103,6 +107,7 @@ class MessageForm extends React.Component {
                     labelPosition="right"
                     />
                     <FileModal
+                      uploadFile={this.uploadFile}
                       modal={modal}
                       closeModal={this.closeModal}
                     />
