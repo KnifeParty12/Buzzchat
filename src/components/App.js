4a83284373b9befd3c2e2ee97d66e79d0c1de37e
= <=>
2019-10-28 10:13:18 -0300

    adding emojis picker into message form

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,60 @@
 import React from "react";
-import {Grid} from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import "./App.css";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 import ColorPanel from "./ColorPanel/ColorPanel";
 import MetaPanel from "./MetaPanel/MetaPanel";
 import Messages from "./Messages/Messages";
 import SidePanel from "./SidePanel/SidePanel";
+import "emoji-mart/css/emoji-mart.css";
 
-const App = ({ currentUser,currentChannel, isPrivateChannel, userPosts, primaryColor,secondaryColor }) => (
-    <Grid
-        columns="equal"
-    className="app"
-    style={{background: secondaryColor}}>
-      <ColorPanel
-          key={currentUser && currentUser.name}
-          currentUser={currentUser}/>
-      <SidePanel
-          key={currentUser && currentUser.uid}
-          currentUser={currentUser}
-          primaryColor={primaryColor}
-      />
+const App = ({
+  currentUser,
+  currentChannel,
+  isPrivateChannel,
+  userPosts,
+  primaryColor,
+  secondaryColor
+}) => (
+  <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
+    <ColorPanel
+      key={currentUser && currentUser.name}
+      currentUser={currentUser}
+    />
+    <SidePanel
+      key={currentUser && currentUser.uid}
+      currentUser={currentUser}
+      primaryColor={primaryColor}
+    />
 
-      <Grid.Column style={{marginLeft: 320 }}>
-        <Messages
-          key={currentChannel && currentChannel.id}
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages
+        key={currentChannel && currentChannel.id}
         currentChannel={currentChannel}
-          currentUser={currentUser}
-          isPrivateChannel={isPrivateChannel}
-        />
-      </Grid.Column>
+        currentUser={currentUser}
+        isPrivateChannel={isPrivateChannel}
+      />
+    </Grid.Column>
 
-      <Grid.Column width={4}>
-        <MetaPanel
+    <Grid.Column width={4}>
+      <MetaPanel
         key={currentChannel && currentChannel.name}
         userPosts={userPosts}
         currentChannel={currentChannel}
         isPrivateChannel={isPrivateChannel}
-        />
-      </Grid.Column>
-    </Grid>
+      />
+    </Grid.Column>
+  </Grid>
 );
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
-    currentChannel: state.channel.currentChannel,
-    isPrivateChannel : state.channel.isPrivateChannel,
-    userPosts: state.channel.userPosts,
-    primaryColor: state.colors.primaryColor,
-    secondaryColor: state.colors.secondaryColor
+  currentChannel: state.channel.currentChannel,
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts,
+  primaryColor: state.colors.primaryColor,
+  secondaryColor: state.colors.secondaryColor
 });
 
 export default connect(mapStateToProps)(App);
